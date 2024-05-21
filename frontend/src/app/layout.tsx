31679592e52896794ff44d017b19ea05cf343b51
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-05-21 21:19:46 +0200

    change: change thirdweb to solana provider

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThirdwebProvider } from "thirdweb/react";
+import '@radix-ui/themes/styles.css';
+import AppWalletProvider from "@/app/AppWalletProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThirdwebProvider>{children}</ThirdwebProvider>
+        <AppWalletProvider>{children}</AppWalletProvider>
       </body>
     </html>
   );
