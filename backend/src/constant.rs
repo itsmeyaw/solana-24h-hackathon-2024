ccb106e9123a9aefc08fff29ef95a3dbcc9ef621
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-05-22 11:29:05 +0200

    save: solana code

--- backend/src/constant.rs
+++ backend/src/constant.rs
@@ -2,3 +2,9 @@ use anchor_lang::prelude::*;
 
 #[constant]
 pub const PROPERTY_SEED: &[u8] = b"property";
+
+#[constant]
+pub const RENT_SEED: &[u8] = b"rent";
+
+#[constant]
+pub const MARKUP: u64 = 1_000_000;
--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, Token, TokenAccount, Transfer};
 
 pub mod constant;
 
@@ -9,46 +10,70 @@ use crate::constant::*;
 declare_id!("DzyyyfGG6RHiDBZo9aacWTGdgycQv8YsTjGZSfNmHpQT");
 
 #[program]
-mod hello_anchor {
+mod solbnb {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
-        ctx.accounts.new_account.data = data;
-        msg!("Changed data to: {}!", data); // Message will show up in the tx logs
+    use anchor_lang::Key;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 
-    pub fn create_property(ctx: Context<CreateProperty>) -> Result<()> {
+    pub fn create_property(
+        ctx: Context<CreateProperty>,
+        name: String,
+        address: String,
+        city: String,
+        zip: String,
+        country: String,
+        price: u64,
+    ) -> Result<Pubkey> {
         let property = &mut ctx.accounts.property;
-        let owner = &mut ctx.accounts.owner;
+        let owner = &ctx.accounts.owner;
+
+        property.name = name;
+        property.address = address;
+        property.zip = zip;
+        property.city = city;
+        property.country = country;
+        property.owner = owner.key();
+        property.price = price;
+
+        msg!("Created property {} by {}", property.key(), owner.key());
+        Ok(property.key())
+    }
+
+    pub fn rent_property(
+        ctx: Context<RentProperty>,
+        property_address: Pubkey,
+        duration: u8,
+    ) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    // We must specify the space in order to initialize an account.
-    // First 8 bytes are default account discriminator,
-    // next 8 bytes come from NewAccount.data being type u64.
-    // (u64 = 64 bits unsigned integer = 8 bytes)
-    #[account(init, payer = signer, space = 8 + 8)]
-    pub new_account: Account<'info, NewAccount>,
+    #[account(
+        init,
+        seeds = [b"nft_mint_authority"],
+        payer = signer,
+        space = 8,
+        bump,
+    )]
+    pub mint_authority: AccountInfo<'info>,
     #[account(mut)]
     pub signer: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
-#[account]
-pub struct NewAccount {
-    data: u64,
-}
-
 #[derive(Accounts)]
+#[instruction()]
 pub struct CreateProperty<'info> {
     #[account(
         init, payer = owner,
         seeds = [PROPERTY_SEED, owner.key().as_ref()],
         bump,
-        space = 8 + (4 + 256) + (4 + 2048) + (4 + 128) + (4 + 128) + (4 + 256)
+        space = 8 + Property::INIT_SPACE
     )]
     pub property: Account<'info, Property>,
 
@@ -58,10 +83,66 @@ pub struct CreateProperty<'info> {
 }
 
 #[account]
+#[derive(InitSpace)]
 pub struct Property {
-    pub property_name: String,  // 4 + 256
-    pub address: String,        // 4 + 2048
-    pub city: String,           // 4 + 128
-    pub zip: String,            // 4 + 128
-    pub country: String,        // 4 + 256
+    pub name: String,    // 4 + 256
+    pub owner: Pubkey,   // 32
+    pub address: String, // 4 + 2048
+    pub city: String,    // 4 + 128
+    pub zip: String,     // 4 + 128
+    pub country: String, // 4 + 256
+    pub price: u64,      // 8
+}
+
+#[derive(Accounts)]
+#[instruction(property_address: Pubkey)]
+pub struct RentProperty<'info> {
+    #[account(
+        mut,
+        seeds = [PROPERTY_SEED, owner.key().as_ref()],
+        bump
+    )]
+    pub property: Account<'info, Property>,
+    #[account(
+        init, payer = owner,
+        seeds = [RENT_SEED, owner.key().as_ref()],
+        bump,
+        space = 8 + RentProof::INIT_SPACE
+    )]
+    pub rent_proof: Account<'info, RentProof>,
+
+    #[account(mut)]
+    pub tenant: Signer<'info>,
+    pub mint: Account<'info, Mint>, // Mint for the NFT
+    #[account(
+        mut,
+        payer = tenant,
+        associated_token::mint = mint,
+        associated_token::authority = tenant
+    )]
+    pub tenant_token_account: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        seeds = [b"nft_mint_authority"],
+        bump,
+    )]
+    pub mint_authority: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct RentProof {
+    pub tenant: Pubkey,             // 32
+    pub property: Pubkey,           // 32
+    pub from: u64,                  // 8
+    pub to: u64,                    // 8
+    pub smart_contract_key: Pubkey, //32
+}
+
+#[error_code]
+pub enum RentError {
+    #[msg("Insufficient funds to rent the property.")]
+    InsufficientFunds,
 }
